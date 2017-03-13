48990becae987f38bd21902391f191545d8bfad1
Lisa Schoofs <lisaschoofs@gmail.com>
2017-03-13 13:20:00 -0500

    adds generate button that appends a div when clicked

--- client.js
+++ client.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
   console.log("Let's do this!");
+  eventListeners();
 });
+
+function eventListeners() {
+  $(".button").on("click", function(){
+    $(".container").append("<div></div>");
+  });
+
+}

18783e24ddc42a7d9cedf7a84d01728dfc13732d
Lisa Schoofs <lisaschoofs@gmail.com>
2017-03-13 13:53:18 -0500

    adds functionality to swap button

--- client.js
+++ client.js
@@ -9,20 +9,22 @@ function eventListeners() {
   $(".button").on("click", function(){
     generateClicks += 1;
     $(".container").append("<div class='red'><p>Clicks = " + generateClicks + "</div>");
-    $(".container").children().last().append("<button id='swap'>Swap</button><button id='delete'>Delete</button>");
+    $(".container").children().last().append("<button class='swap'>Swap</button><button id='delete'>Delete</button>");
   });
 
+//sets up listener to switch background color to yellow
+  $(".container").on("click", ".swap", function(){
+      $(this).parent().addClass("yellow");
+  });
   //sets up delete button listener
   $(".container").on("click", "#delete", function(){
-        $(this).parent().remove(); //remove parent?
+        $(this).parent().remove();
   });
 }
 
 
 
 // Follow the steps below to complete the code challenge:
-//
-// Inside the <div> element you created in step two, append two <button> elements. The text of the two buttons should read Swap and Delete.
 //
 // Clicking a Swap <button> should change its parent background-color from Red to Yellow.
 //

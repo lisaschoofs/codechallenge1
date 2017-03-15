bc6b10af64c0ac5f31adf30581016a6c72564441
Lisa Schoofs <lisaschoofs@gmail.com>
2017-03-14 21:11:24 -0500

    updates swap button to toggle between red and yellow classes

--- client.js
+++ client.js
@@ -10,12 +10,13 @@ function eventListeners() {
   $(".button").on("click", function(){
     generateClicks += 1;
     $(".container").append("<div class='red'><p>Clicks = " + generateClicks + "</div>");
-    $(".container").children().last().append("<button class='swap'>Swap</button><button id='delete'>Delete</button>");
+    $(".container").children().last().
+    append("<button class='swap'>Swap</button><button id='delete'>Delete</button>");
   });
 
-//sets up listener to switch background color to yellow
+//sets up listener to switch background color between red & yellow
   $(".container").on("click", ".swap", function(){
-      $(this).parent().addClass("yellow");
+      $(this).parent().toggleClass("yellow", "red");
   });
   //sets up delete button listener
   $(".container").on("click", "#delete", function(){
